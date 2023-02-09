145ed039ff6bd748594c753b4a43d599380ed3f9
Dmitriy Smirnov <git@d-sm.ru>
2023-02-09 22:36:51 +0300

    task1 completed

---
+++ netology_pd71_db_hw2_task1.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS musical_artist (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS musical_album (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  year_of_release INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS musical_album_artist (
+  musical_album_id INTEGER REFERENCES musical_album(id),
+  musical_artist_id INTEGER REFERENCES musical_artist(id),
+  CONSTRAINT pk_musical_album_artist PRIMARY KEY (musical_album_id, musical_artist_id)
+);
+
+CREATE TABLE IF NOT EXISTS musical_genre (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS musical_artist_genre (
+  musical_artist_id INTEGER REFERENCES musical_artist(id),
+  musical_genre_id INTEGER REFERENCES musical_genre(id),
+  CONSTRAINT pk_musical_artist_genre PRIMARY KEY (musical_artist_id, musical_genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS musical_track (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  duration INTEGER NOT NULL,
+  musical_album_id INTEGER NOT NULL REFERENCES musical_album(id)
+);
+
+CREATE TABLE IF NOT EXISTS music_collection (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  year_of_release INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS musical_track_collection (
+  musical_track_id INTEGER REFERENCES musical_track(id),
+  music_collection_id INTEGER REFERENCES music_collection(id),
+  CONSTRAINT pk_musical_track_collection PRIMARY KEY (musical_track_id, music_collection_id)
+);
